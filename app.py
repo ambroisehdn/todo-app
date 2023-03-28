60acaed320eea0ada6986755d08ff2b7cd30f27f
ambroisehdn <ambroise.hound@gmail.com>
2023-03-28 06:48:47 +0100

    add database connection

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 from flask_restful import Resource, Api
 from user.app import User,UserList
+from config import DevelopmentConfig as devConf
 
 app = Flask(__name__)
 api = Api(app)
+
+connection = psycopg2.connect(devConf.SQLALCHEMY_DATABASE_URI)
+
+
+@app.route('/')
+def index():
+    return jsonify({"message": "flask app :) "})
 
 api.add_resource(UserList, '/user')
 api.add_resource(User, '/user/<int:id>',endpoint='user_ep')
 
 if __name__ == '__main__':
-    app.run(debug=config.DevelopmentConfig().FLASK_DEBUG)
+    app.run(debug=devConf.FLASK_DEBUG)
